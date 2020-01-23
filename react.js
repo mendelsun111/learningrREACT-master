ff792c64a26866a543836b38b17f52a79753943d
mendelsun111 <mendelsun21@gmail.com>
2020-01-23 17:35:02 -0500

    Components done on same file

--- react.js
+++ react.js
@@ -7,8 +7,8 @@ ReactDOM.render(<div><h1>Hello World!</h1><p>This is a paragraph</p></div>, docu
 
 //render unordered list
 
-import React from "react";
-import ReactDOM from "react-dom"
+//import React from "react";
+//import ReactDOM from "react-dom"
 
 //capital letter for first letter if its a component
 function myApp() {
@@ -23,3 +23,30 @@ function myApp() {
 
 ReactDOM.render(<MyApp/>
 , document.getElementById("root"))
+
+// Objectives:
+// 1. Set up the basic React code from scratch
+// 2. Create a functional component called MyInfo that returns the following UI:
+    // a. An h1 with your name
+    // b. A paragraph with a little blurb about yourself
+    // c. An ordered or unordered list of the top 3 vacation spots you'd like to visit
+// 3. Render an instance of that functional component to the browser
+// Extra challenge: learn on your own (Google!) how you can add some style to your page.
+// (We will also cover this in an upcoming lesson).
+
+//import React from "react"
+//import ReactDOM from "react-dom"
+
+function MyInfo(){
+  return <div>
+    <h1> mendel </h1>
+    <p> my name is mendel, i am going to be the top at whatever i want to do</p>
+    <ol>
+    <li>Korea</li>
+    <li>China</li>
+    <li>Japan</li>
+    </ol>
+   </div> 
+}
+
+ReactDOM.render(<MyInfo/>,document.getElementById("root"));
