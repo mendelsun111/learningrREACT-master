64182ec2ee3c3a39d2d230a78a39a60be7487b88
mendelsun111 <mendelsun21@gmail.com>
2020-01-23 18:11:39 -0500

    Child parent components

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Footer from "./Footer"
+
+function App() {
+    <div>
+        <nav>
+        <h1> call me big boi</h1>
+        <ul> 
+            <li>u sure?</li>
+            <li>live up</li>
+            <li>funnyman</li>
+        </ul>
+        </nav>
+        <main>
+            <p> This is where my content will go</p>
+        </main>
+        <Footer />
+    </div>
+}
+
+
+export default App
